12dcb9558edf032f3377d4984a9ccc3be518ebf7
Lia Maulida As Sa'adah <liamaulida55@gmail.com>
2025-04-06 22:05:52 +0700

    Membuat tampilan about

--- app/src/main/java/com/liamaulida0026/assessmentmobpro1/navigation/NavGraph.kt
+++ app/src/main/java/com/liamaulida0026/assessmentmobpro1/navigation/NavGraph.kt
@@ -5,6 +5,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.liamaulida0026.assessmentmobpro1.ui.screen.AboutScreen
 import com.liamaulida0026.assessmentmobpro1.ui.screen.MainScreen
 
 @Composable
@@ -15,6 +16,9 @@ fun SetUpNavGraph (navController: NavHostController = rememberNavController()) {
     ) {
         composable(route = Screen.Home.route) {
             MainScreen()
+        }
+        composable(route = Screen.About.route) {
+            AboutScreen()
         }
     }
 }
--- app/src/main/java/com/liamaulida0026/assessmentmobpro1/navigation/Screen.kt
+++ app/src/main/java/com/liamaulida0026/assessmentmobpro1/navigation/Screen.kt
@@ -2,4 +2,5 @@ package com.liamaulida0026.assessmentmobpro1.navigation
 
 sealed class Screen(val route: String) {
     data object Home: Screen("mainScreen")
+    data object About: Screen("aboutScreen")
 }
