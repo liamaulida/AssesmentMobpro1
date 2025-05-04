a71a9aaca6315c96732601007b68dceb0c2d7b08
Lia Maulida As Sa'adah <liamaulida55@gmail.com>
2025-05-04 21:41:48 +0700

    Membuat tampilan list

--- app/src/main/java/com/liamaulida0026/assessmentmobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/liamaulida0026/assessmentmobpro1/ui/screen/MainScreen.kt
@@ -1,8 +1,15 @@
 package com.liamaulida0026.assessmentmobpro1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -11,8 +18,14 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.liamaulida0026.assessmentmobpro1.R
+import com.liamaulida0026.assessmentmobpro1.model.Catatan
 import com.liamaulida0026.assessmentmobpro1.ui.theme.AssessmentMobpro1Theme
 
 
@@ -38,10 +51,43 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Android",
-        modifier = modifier
-    )
+    val viewModel: MainViewModel = viewModel()
+    val data = viewModel.data
+
+    LazyColumn(
+        modifier = modifier.fillMaxSize()
+    ) {
+        items(data) {
+            ListItem(catatan = it)
+            HorizontalDivider()
+        }
+    }
+}
+
+@Composable
+fun ListItem(catatan: Catatan) {
+    Column(
+        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = catatan.judul,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold
+            )
+        Text(
+            text = catatan.berat.toString(),
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+            )
+        Text(
+            text = catatan.satuan,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+            )
+        Text(text = catatan.kategori)
+    }
 }
 
 @Preview(showBackground = true)
