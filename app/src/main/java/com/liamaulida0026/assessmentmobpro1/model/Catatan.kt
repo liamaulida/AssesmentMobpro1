773f8396d6b0eb4d6a9fbf10bc504f027d2b898d
Lia Maulida As Sa'adah <liamaulida55@gmail.com>
2025-05-04 21:41:19 +0700

    Menyiapkan data di ViewModel

---
+++ app/src/main/java/com/liamaulida0026/assessmentmobpro1/model/Catatan.kt
@@ -0,0 +1,9 @@
+package com.liamaulida0026.assessmentmobpro1.model
+
+data class Catatan(
+    val id: Long,
+    val judul: String,
+    val berat: Double,
+    val satuan: String,
+    val kategori: String
+)
---
+++ app/src/main/java/com/liamaulida0026/assessmentmobpro1/ui/screen/MainViewModel.kt
@@ -0,0 +1,58 @@
+package com.liamaulida0026.assessmentmobpro1.ui.screen
+
+import androidx.lifecycle.ViewModel
+import com.liamaulida0026.assessmentmobpro1.model.Catatan
+
+class MainViewModel : ViewModel() {
+    val data = listOf(
+        Catatan(
+            id = 1,
+            judul = "Pisang",
+            berat = 1.2,
+            satuan = "Kilogram",
+            kategori = "Buah"
+        ),
+        Catatan(
+            id = 2,
+            judul = "Wortel",
+            berat = 0.5,
+            satuan = "Kilogram",
+            kategori = "Sayur"
+        ),
+        Catatan(
+            id = 3,
+            judul = "Dada Ayam",
+            berat = 1.0,
+            satuan = "Kilogram",
+            kategori = "Daging"
+        ),
+        Catatan(
+            id = 4,
+            judul = "Beras",
+            berat = 2.0,
+            satuan = "Kilogram",
+            kategori = "Lainnya"
+        ),
+        Catatan(
+            id = 5,
+            judul = "Apel",
+            berat = 1.0,
+            satuan = "Kilogram",
+            kategori = "Buah"
+        ),
+        Catatan(
+            id = 6,
+            judul = "Bayam",
+            berat = 0.3,
+            satuan = "Kilogram",
+            kategori = "Sayur"
+        ),
+        Catatan(
+            id = 7,
+            judul = "Mie Kering",
+            berat = 2.0,
+            satuan = "Kilogram",
+            kategori = "Lainnya"
+        )
+    )
+}
