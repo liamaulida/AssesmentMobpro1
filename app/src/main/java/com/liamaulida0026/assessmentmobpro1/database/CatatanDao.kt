494ee293d1b6a0216b9281405f394f4b1e924845
Lia Maulida As Sa'adah <liamaulida55@gmail.com>
2025-05-07 17:00:54 +0700

    Menghapus data dari database

--- app/src/main/java/com/liamaulida0026/assessmentmobpro1/database/CatatanDao.kt
+++ app/src/main/java/com/liamaulida0026/assessmentmobpro1/database/CatatanDao.kt
@@ -21,4 +21,9 @@ interface CatatanDao {
 
     @Query("SELECT * FROM catatan WHERE id = :id")
     suspend fun getCatatanById(id: Long): Catatan?
+
+    @Query("DELETE FROM catatan WHERE id = :id")
+    suspend fun deleteById(id: Long)
+
+
 }
--- app/src/main/java/com/liamaulida0026/assessmentmobpro1/ui/screen/DetailScreen.kt
+++ app/src/main/java/com/liamaulida0026/assessmentmobpro1/ui/screen/DetailScreen.kt
@@ -13,7 +13,9 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.outlined.Check
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -125,6 +127,12 @@ fun DetailScreen(navController: NavHostController, id: Long? = null) {
                             tint = MaterialTheme.colorScheme.primary
                         )
                     }
+                    if (id != null) {
+                        DeleteAction {
+                            viewModel.delete(id)
+                            navController.popBackStack()
+                        }
+                    }
                 }
             )
         }
@@ -144,6 +152,33 @@ fun DetailScreen(navController: NavHostController, id: Long? = null) {
             onExpandedChange = { expanded = it },
             modifier = Modifier.padding(padding)
         )
+    }
+}
+
+@Composable
+fun DeleteAction(delete: () -> Unit) {
+    var expanded by remember { mutableStateOf(false) }
+
+    IconButton(onClick = { expanded = true }) {
+        Icon(
+            imageVector = Icons.Filled.MoreVert,
+            contentDescription = stringResource(R.string.opsi_lainnya),
+            tint = MaterialTheme.colorScheme.primary
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                text = {
+                    Text(text = stringResource(id = R.string.hapus))
+                },
+                onClick = {
+                    expanded = false
+                    delete()
+                }
+            )
+        }
     }
 }
 
