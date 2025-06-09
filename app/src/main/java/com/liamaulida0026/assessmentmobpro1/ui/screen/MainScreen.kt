9b5b3f0a40e8eafd74c5d61225f91d13e729fe81
Lia Maulida As Sa'adah <liamaulida55@gmail.com>
2025-06-09 21:02:56 +0700

    Menambahkan penanganan error jika gambar gagal di-load

--- app/src/main/java/com/liamaulida0026/assessmentmobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/liamaulida0026/assessmentmobpro1/ui/screen/MainScreen.kt
@@ -127,6 +127,7 @@ fun ListItem(buku: Buku) {
             contentDescription = stringResource(R.string.gambar, buku.judulBuku),
             contentScale = ContentScale.Crop,
             placeholder = painterResource(id = R.drawable.loading_img),
+            error = painterResource(id = R.drawable.broken_img),
             modifier = Modifier.fillMaxWidth().padding(4.dp)
         )
         Column(
