0a47af46593d09dfc3df9812341ef3e40698d27d
Lia Maulida As Sa'adah <liamaulida55@gmail.com>
2025-06-09 20:10:08 +0700

    Memproses data string sebagai JSON

---
+++ app/src/main/java/com/liamaulida0026/assessmentmobpro1/model/Buku.kt
@@ -0,0 +1,8 @@
+package com.liamaulida0026.assessmentmobpro1.model
+
+data class Buku(
+    val judulBuku: String,
+    val penulisBuku: String,
+    val reviewBuku: String,
+    val imageId: String,
+)
--- app/src/main/java/com/liamaulida0026/assessmentmobpro1/network/BukuApiService.kt
+++ app/src/main/java/com/liamaulida0026/assessmentmobpro1/network/BukuApiService.kt
@@ -1,19 +1,26 @@
 package com.liamaulida0026.assessmentmobpro1.network
 
+import com.liamaulida0026.assessmentmobpro1.model.Buku
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://store.sthresearch.site/"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
 interface BukuApiService {
     @GET("buku.php")
-    suspend fun getBuku(): String
+    suspend fun getBuku(): List<Buku>
 }
 
 object BukuApi {
