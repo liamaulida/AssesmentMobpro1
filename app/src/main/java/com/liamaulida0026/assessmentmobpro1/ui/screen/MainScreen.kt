b0abdf10f36a366496f71cd5d38fdb3e666703ba
Lia Maulida As Sa'adah <liamaulida55@gmail.com>
2025-06-10 19:44:56 +0700

    Membuat fitur logout

--- app/src/main/java/com/liamaulida0026/assessmentmobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/liamaulida0026/assessmentmobpro1/ui/screen/MainScreen.kt
@@ -51,10 +51,12 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.credentials.ClearCredentialStateRequest
 import androidx.credentials.CredentialManager
 import androidx.credentials.CustomCredential
 import androidx.credentials.GetCredentialRequest
 import androidx.credentials.GetCredentialResponse
+import androidx.credentials.exceptions.ClearCredentialException
 import androidx.credentials.exceptions.GetCredentialException
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
@@ -143,6 +145,7 @@ fun MainScreen() {
                 user = user,
                 onDismissRequest = { showDialog = false }
             ) {
+                CoroutineScope(Dispatchers.IO).launch { signOut(context, dataStore) }
                 showDialog = false
             }
         }
@@ -265,6 +268,18 @@ private suspend fun handleSignIn(
         }
     } else {
         Log.e("SIGN-IN", "Error: unrecognized custom credential type.")
+    }
+}
+
+private suspend fun signOut(context: Context, dataStore: UserDataStore) {
+    try {
+        val credentialManager = CredentialManager.create(context)
+        credentialManager.clearCredentialState(
+            ClearCredentialStateRequest()
+        )
+        dataStore.saveData(User())
+    } catch (e: ClearCredentialException) {
+        Log.e("SIGN-IN", "Error: ${e.errorMessage}")
     }
 }
 
--- app/src/main/java/com/liamaulida0026/assessmentmobpro1/ui/screen/ProfilDialog.kt
+++ app/src/main/java/com/liamaulida0026/assessmentmobpro1/ui/screen/ProfilDialog.kt
@@ -1,19 +1,23 @@
 package com.liamaulida0026.assessmentmobpro1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -34,7 +38,7 @@ fun ProfilDialog(
     user: User,
     onDismissRequest: () -> Unit,
     onConfirmation: () -> Unit
-    ) {
+) {
     Dialog(onDismissRequest = { onDismissRequest() }) {
         Card(
             modifier = Modifier.padding(16.dp),
@@ -53,14 +57,14 @@ fun ProfilDialog(
                     contentScale = ContentScale.Crop,
                     placeholder = painterResource(id = R.drawable.loading_img),
                     error = painterResource(id = R.drawable.broken_img),
-                    modifier = Modifier.size(100.dp)
+                    modifier = Modifier.size(100.dp).clip(CircleShape)
                 )
                 Text(
                     text = user.name,
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis,
                     fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(top =16.dp)
+                    modifier = Modifier.padding(top = 16.dp)
                 )
                 Text(
                     text = user.email,
@@ -77,6 +81,16 @@ fun ProfilDialog(
                     ) {
                         Text(stringResource(R.string.tutup))
                     }
+                    OutlinedButton(
+                        onClick = { onConfirmation() },
+                        modifier = Modifier.padding(8.dp),
+                        border = BorderStroke(1.dp, MaterialTheme.colorScheme.error)
+                    ) {
+                        Text(
+                            text = stringResource(R.string.logout),
+                            color = MaterialTheme.colorScheme.error
+                        )
+                    }
                 }
             }
         }
@@ -86,7 +100,7 @@ fun ProfilDialog(
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
-fun DialogPreview() {
+fun DialogPrefiew() {
     AssessmentMobpro1Theme {
         ProfilDialog(
             user = User("Lia Maulida As Sa'adah", "liamaulidaa15@gmail.com", ""),
